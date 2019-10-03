5b98fceaeecab06a148f0ddd20e5df28d2c45327
r12w4n <48220263+r12w4n@users.noreply.github.com>
2019-10-03 15:16:46 +0530

    Update PiMAP.sh

--- PiMAP.sh
+++ PiMAP.sh
@@ -12,7 +12,8 @@ mkdir $name-nmap-reports && cd $name-nmap-reports
 echo "${reset}"
 wget -p /usr/share/nmap/scripts https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
 git clone https://github.com/vulnersCom/nmap-vulners.git /usr/share/nmap/scripts
-wget https://raw.githubusercontent.com/r12w4n/AVIATO-CLI/master/nmap-bootstrap.xsl
+wget https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl
+#wget https://raw.githubusercontent.com/r12w4n/AVIATO-CLI/master/nmap-bootstrap.xsl
 wget https://github.com/r12w4n/AVIATO-CLI/raw/master/DisplayDirectoryContents.zip
 unzip DisplayDirectoryContents.zip -d /var/www/html/$name
 
