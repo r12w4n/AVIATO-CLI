5a078a0339bd023199a077a297647b1e480a12d2
r12w4n <48220263+r12w4n@users.noreply.github.com>
2019-10-03 11:11:24 +0530

    Initial Upload

---
+++ PiMAP.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+#echo "${red}red text ${green}green text${reset}"
+sudo apt-get install ccze -y
+read -p "${red}Enter the Project Name : " name
+read -p "${red}Enter IP Address/ IP Range : " iprange
+mkdir $name-nmap-reports && cd $name-nmap-reports
+echo "${reset}"
+wget https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl
+git clone https://github.com/ralphbean/ansi2html.git && cd ansi2html && chmod +x setup.py && ./setup.py install && cd ..
+read -p "Start Scanning ? y/n " ss
+	while true; do echo -n .; sleep 1; done &
+	trap 'kill $!' SIGTERM SIGKILL
+
+	if [[ $ss = "y" ]]; then
+
+	echo "${green}Scanning Started ${reset}"
+	nmap -A -oA $name --stylesheet nmap-bootstrap.xsl $iprange | ccze -A | ansi2html > $name.html
+	echo "${green}Main Scan Done ${reset}"
+
+	echo "${green}Logging Live Host ${reset}"
+	nmap -oG  $name-grepable.txt $iprange
+	cat $name-grepable.txt | grep Up | cut -d ' ' -f 2 | sort -u > $name-livehost.txt
+	cat $name-livehost.txt
+	echo "${green}Logging Live Host Done ${reset}"
+
+	echo "${green}Scanning for vulnerabilities CVE in live host ${reset}"
+	nmap -oA $name-vulners --stylesheet nmap-bootstrap.xsl -sV $iprange --script vulners.nse | ccze -A | ansi2html > $name-vulners.html
+	echo "${green}Vulnerability Scanning Done ${reset}"
+
+	read -p "Do you want perform Advance Vulnerabilty Scan? y/n" avs
+	if [[ $avs = "y" ]]; then
+		echo "${green}Advance Vulnerabilty Scan Started ${reset}"
+		nmap -oA $name-advance-vuln --stylesheet nmap-bootstrap.xsl -sV $iprange --script=vulscan/vulscan.nse -script-args vulscanoutput=details | ccze -A | ansi2html > $name-advance-vuln.html
+		echo "${green}Advance Vulnerabilty Scan Completed ${reset}"
+		rm -rf ansi2html nmap-bootstrap.xsl
+                mkdir /var/www/html/$name
+                cp `find . -type f \( -iname \*.xml -o -iname \*.html \)` /var/www/html/$name/
+                kill $!
+                exit 0
+	fi
+		rm -rf ansi2html nmap-bootstrap.xsl
+                mkdir /var/www/html/$name
+                cp `find . -type f \( -iname \*.xml -o -iname \*.html \)` /var/www/html/$name/
+                kill $!
+                exit 0
+
+fi
+	kill $!
+	exit 0
