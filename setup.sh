abae96356bae9a9024954484b3ed367e42650f97
r12w4n <48220263+r12w4n@users.noreply.github.com>
2020-02-13 00:08:24 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -23,6 +23,8 @@ echo -e "${GREEN}[+] Setting things up.${RESET}"
     sudo apt clean
     sudo apt install -y gcc g++ make libpcap-dev xsltproc
     sudo apt install python3-pip -y 
+    sudo apt install php -y 
+    sudo apt install python-pip -y   
     sudo pip install ansi2html
     sudo apt install ccze -y
     sudo apt-get install nmap -y
@@ -30,7 +32,31 @@ echo -e "${GREEN}[+] Setting things up.${RESET}"
     sudo pip install XlsxWriter
     wget https://raw.githubusercontent.com/mrschyte/nmap-converter/master/nmap-converter.py
     chmod +x nmap-converter.py
-    #wget -P /usr/share/nmap/scripts/ https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
+  	FILE=directory-list.zip
+  	if [ -f "$FILE" ]; then
+    	echo "Directory Listing Zip file found"
+	Done
+	else
+	echo "$FILE does not exist"
+	echo "Downloading directory-listing.zip"
+	wget https://github.com/r12w4n/AVIATO-CLI/raw/master/directory-list.zip
+	echo "Done"
+	fi
+
+
+	FILE2=snmp-community.txt
+        if [ -f "$FILE2" ]; then
+        echo "SNMP Community String List Found"
+        Done
+        else
+        echo "$FILE2 does not exist"
+        echo "Downloading SNMP Community String List Found"
+        wget https://raw.githubusercontent.com/r12w4n/AVIATO-CLI/master/snmp-community.txt
+        echo "Done"
+        fi
+   
+ 
+   #wget -P /usr/share/nmap/scripts/ https://raw.githubusercontent.com/vulnersCom/nmap-vulners/master/vulners.nse
     #cd /usr/share/nmap/scripts/ && git clone https://github.com/scipag/vulscan.git
     vulners="/usr/share/nmap/scripts/vulners.nse"
 	if [ ! -f "$vulners" ]
